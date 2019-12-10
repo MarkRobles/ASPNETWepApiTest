33c1c242b2f1b56bc7d783086bd21c27d520d0a7
Marcos Ivan Robles Hernandez <marcos92robles@gmail.com>
2019-12-10 14:11:23 -0600

    Securing ASP Web API book

---
+++ TalentManager/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TalentManager.Controllers.MessageHandlers;
+
+namespace TalentManager
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.MessageHandlers.Add(new MyHandler());
+            //No enviar  stack trace al cliente
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Never;
+        }
+    }
+}
---
+++ TalentManager/Controllers/MessageHandlers/MyHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace TalentManager.Controllers.MessageHandlers
+{
+    public class MyHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+        {
+
+            if (request.Method == HttpMethod.Post && request.Headers.Contains("X-HTTP-Method-Override"))
+            {
+                var method = request.Headers.GetValues("X-HTTP-Method-Override").FirstOrDefault();
+                bool isPut = String.Equals(method, "PUT", StringComparison.OrdinalIgnoreCase);
+                bool isDelete = String.Equals(method, "DELETE", StringComparison.OrdinalIgnoreCase);
+                if (isPut || isDelete)
+                {
+                    request.Method = new HttpMethod(method);
+                }
+            }else
+            {
+                // Inspect and do your stuff with request here
+                // If you are not happy for any reason,
+                // you can reject the request right here like this
+                bool isBadRequest = false;
+                if (isBadRequest)
+                    return request.CreateResponse(HttpStatusCode.BadRequest);
+                var response = await base.SendAsync(request, cancellationToken);
+                // Inspect and do your stuff with response here
+                return response;
+            }
+            return await base.SendAsync(request, cancellationToken);
+          
+        }
+    }
+}
