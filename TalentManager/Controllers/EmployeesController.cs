3450fcbde970bd33442d8d61997bef97778c28e9
Marcos Ivan Robles Hernandez <marcos92robles@gmail.com>
2019-12-11 16:54:29 -0600

    Agregar Claim-BasedSecurity

--- TalentManager/Controllers/EmployeesController.cs
+++ TalentManager/Controllers/EmployeesController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Web.Http;
 using TalentManager.CustomActionFilters.Etags;
 using TalentManager.Etags;
@@ -11,6 +14,18 @@ using TalentManager.Models;
 
 namespace TalentManager.Controllers
 {
+
+    public static class PrincipalHelper
+    {
+        public static bool CheckAccess(this IPrincipal principal, string resource, string action,   IList<Claim> resourceClaims)
+        {
+            var context = new AuthorizationContext(principal as ClaimsPrincipal,
+            resource, action);
+            resourceClaims.ToList().ForEach(c => context.Resource.Add(c));
+            var config = new IdentityConfiguration();
+            return config.ClaimsAuthorizationManager.CheckAccess(context);
+        }
+    }
     public class EmployeesController : ApiController
     {
         [EnableETag]
@@ -63,6 +78,11 @@ namespace TalentManager.Controllers
                     };
             var response = Request.CreateResponse<IEnumerable<Employee>>
             (HttpStatusCode.OK, employees);
+
+            //Agregar CORS
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            //Habilitar cache 
             response.Headers.CacheControl = new CacheControlHeaderValue()
             {
                 MaxAge = TimeSpan.FromSeconds(6),
@@ -74,6 +94,25 @@ namespace TalentManager.Controllers
 
         [ConcurrencyChecker]
         public void Put(Employee employee) { }
+
+
+        public HttpResponseMessage Delete(int id)
+        {
+            // Based on ID, retrieve employee details and create the list of resource claims
+            var employeeClaims = new List<Claim>()
+                {
+                new Claim(ClaimTypes.Country, "US"),
+                new Claim("http://badri/claims/department", "Engineering")
+                };
+
+            if (User.CheckAccess("Employee", "Delete", employeeClaims))
+            {
+                //repository.Remove(id);
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+            else
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+        }
 
 
 
